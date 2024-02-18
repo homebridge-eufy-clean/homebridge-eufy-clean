9ee6392df99b11181303979afc9b4cd47c074ec5
Henry Gelderbloem <hgelderbloem@icloud.com>
2024-02-18 20:18:09 +0000

    Initial version of the eufy Clean RoboVac Plugin

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,165 @@
-import { API } from 'homebridge';
+/* eslint-disable max-len */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  API,
+  AccessoryConfig,
+  AccessoryPlugin,
+  Logging,
+  Service,
+  HAP,
+  CharacteristicEventTypes,
+  CharacteristicGetCallback,
+  CharacteristicValue,
+  CharacteristicSetCallback,
+} from 'homebridge';
+
+let hap: HAP;
 
 import { PLATFORM_NAME } from './settings';
-import { ExampleHomebridgePlatform } from './platform';
-
+import { EufyRoboVacPlatform } from './platform';
+import {
+  RoboVac,
+  WorkStatus,
+} from './robovac';
 
 /**
  * This method registers the platform with Homebridge
  */
 export = (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
+  api.registerPlatform(PLATFORM_NAME, EufyRoboVacPlatform);
 };
 
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
+class EufyCleanRoboVacAccessory implements AccessoryPlugin {
+  private readonly log: Logging;
+  private readonly name: string;
+
+  private readonly vacuumService: Service;
+  private readonly informationService: Service;
+  private roboVac!: RoboVac;
+  private readonly config: { deviceId: any; localKey: any; ip: any };
+  private readonly debugLog: boolean;
+  services: Service[];
+
+  constructor(log: Logging, config: AccessoryConfig, api: API) {
+    this.log = log;
+    this.name = config.name || 'Eufy RoboVac';
+
+    this.debugLog = config.debugLog;
+    this.config = {
+      deviceId: config.deviceId,
+      localKey: config.localKey,
+      ip: config.ip,
+    };
+    this.services = [];
+
+    this.vacuumService = config.useSwitchService ? new hap.Service.Switch(this.name, 'vacuum') : new hap.Service.Fan(this.name, 'vacuum');
+    this.vacuumService.getCharacteristic(hap.Characteristic.On)
+      .on(CharacteristicEventTypes.GET, this.getCleanState.bind(this))
+      .on(CharacteristicEventTypes.SET, this.setCleanState.bind(this));
+    this.services.push(this.vacuumService);
+
+    this.informationService = new hap.Service.AccessoryInformation()
+      .setCharacteristic(hap.Characteristic.Manufacturer, 'Eufy')
+      .setCharacteristic(hap.Characteristic.Model, 'RoboVac');
+    this.services.push(this.informationService);
+
+    this.setup();
+    log.info(`${this.name} finished initializing!`);
+  }
+
+  async setup() {
+    this.roboVac = new RoboVac(this.config, this.debugLog);
+    return await this.roboVac.getStatuses();
+  }
+
+  async getCleanState(callback: CharacteristicGetCallback) {
+    let cleanState;
+    this.log.debug(`getCleanState  for ${this.name}`);
+
+    try {
+      cleanState = await this.roboVac.getPlayPause(true);
+      this.log.debug(`getCleanState for ${this.name} returned ${cleanState}`);
+      callback(undefined, cleanState);
+    } catch(e) {
+      await this.setup();
+      this.getCleanState(callback);
+    }
+  }
+
+  async setCleanState(state: CharacteristicValue, callback: CharacteristicSetCallback) {
+    this.log.debug(`setCleanState for ${this.name} set to ${state}`);
+    await this.roboVac.setPlayPause(state as boolean);
+    if(!state) {
+      await sleep(2000);
+      await this.roboVac.goHome();
+    }
+    callback();
+  }
+
+  async getBatteryLevel(callback: CharacteristicGetCallback) {
+    this.log.debug(`getBatteryLevel for ${this.name}`);
+
+    try {
+      callback(null, await this.roboVac.getBatteryLevel());
+    } catch(e) {
+      this.log.debug(`getBatteryLevel error; ${e}`);
+      callback(null, 0); // push 0 if error, getCleanState will call setup function again
+    }
+  }
+
+  async getChargingState(callback: CharacteristicGetCallback) {
+    this.log.debug(`getChargingState for ${this.name}`);
+
+    try {
+      callback(null, (await this.roboVac.getWorkStatus() === WorkStatus.CHARGING) ? hap.Characteristic.ChargingState.CHARGING : hap.Characteristic.ChargingState.NOT_CHARGEABLE);
+    } catch(e) {
+      this.log.debug(`getChargingState error; ${e}`);
+      callback(null, false); // push not charging if error, getCleanState will call setup function again
+    }
+  }
+
+  async getStatusLowBattery(callback: CharacteristicGetCallback) {
+    try {
+      callback(null, (await this.roboVac.getBatteryLevel() < 30) ? hap.Characteristic.StatusLowBattery.BATTERY_LEVEL_LOW : hap.Characteristic.StatusLowBattery.BATTERY_LEVEL_NORMAL);
+    } catch(e) {
+      callback(null, false); // push not low battery if error, getCleanState will call setup function again
+
+    }
+  }
+
+  async getFindRobot(callback: CharacteristicGetCallback) {
+    this.log.debug(`getFindRobot for ${this.name}`);
+
+    try {
+      callback(null, await this.roboVac.getFindRobot());
+    } catch(e) {
+      callback(null, false); // push false for find switch if error, getCleanState will call setup function again
+    }
+  }
+
+  async setFindRobot(state: CharacteristicValue, callback: CharacteristicSetCallback) {
+    this.log.debug(`setFindRobot for ${this.name} set to ${state}`);
+    await this.roboVac.setFindRobot(state as boolean);
+    callback();
+  }
+
+  async getErrorStatus(callback: CharacteristicGetCallback) {
+    try {
+      callback(null, (await this.roboVac.getErrorCode() === 'no_error') ? false : true);
+    } catch(e) {
+      callback(null, false); // push false for getErrorStatus if error, getCleanState will call setup function again
+    }
+  }
+
+  identify(): void {
+    this.log('Identify!');
+  }
+
+  getServices(): Service[] {
+    return this.services;
+  }
+}
---
+++ src/robovac/index.ts
@@ -0,0 +1,334 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-console */
+import tuyAPI from 'tuyapi';
+
+export class RoboVac {
+
+  api: any;
+
+  PLAY_PAUSE = '2';
+  DIRECTION = '3';
+  WORK_MODE = '5';
+  WORK_STATUS = '15';
+  GO_HOME = '101';
+  CLEAN_SPEED = '102';
+  FIND_ROBOT = '103';
+  BATTERY_LEVEL = '104';
+  ERROR_CODE = '106';
+
+  connected = false;
+  debugLog: boolean;
+
+  statuses: StatusResponse | null = null;
+  lastStatusUpdate: number | null = null;
+  maxStatusUpdateAge: number = 1000 * (1 * 30); //30 Seconds
+  timeoutDuration: number;
+
+  constructor(config: RoboVacConfig, debugLog = false, timeoutDuration = 2) {
+    this.debugLog = debugLog;
+    if (!config.deviceId) {
+      throw new Error('You must pass through deviceId');
+    }
+    this.api = new tuyAPI(
+      {
+        id: config.deviceId,
+        key: config.localKey,
+        ip: config.ip,
+        port: config.port || 6668,
+        version: '3.3',
+        issueRefreshOnConnect: true,
+      },
+    );
+    this.timeoutDuration = timeoutDuration;
+
+    this.api.on('error', (error: any) => {
+      if (debugLog) {
+        console.error('Robovac Error', JSON.stringify(error, null, 4));
+      }
+    });
+
+    this.api.on('connected', () => {
+      this.connected = true;
+      if (debugLog) {
+        console.log('Connected!');
+      }
+    });
+
+    this.api.on('disconnected', () => {
+      this.connected = false;
+      if (debugLog) {
+        console.log('Disconnected!');
+      }
+    });
+
+    this.api.on('dp-refresh', data => {
+      if (debugLog) {
+        console.log('DP_REFRESH data from device: ', data);
+        console.log('Status Updated!');
+      }
+    });
+
+    this.api.on('data', (data: StatusResponse) => {
+      if (debugLog) {
+        console.log('Data from device:', data);
+        console.log('Status Updated!');
+      }
+    });
+
+  }
+
+  async connect() {
+    if (!this.connected) {
+      if (this.debugLog) {
+        console.log('Connecting...');
+      }
+      await this.api.connect();
+    }
+  }
+
+  async disconnect() {
+    if (this.debugLog) {
+      console.log('Disconnecting...');
+    }
+    await this.api.disconnect();
+  }
+
+  async doWork(work: () => Promise<any>): Promise<any> {
+    if (!this.api.device.id || !this.api.device.ip) {
+      if (this.debugLog) {
+        console.log('Looking for device...');
+      }
+      try {
+        await this.api.find({ timeout: this.timeoutDuration });
+        if (this.debugLog) {
+          console.log(`Found device ${this.api.device.id} at ${this.api.device.ip}`);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    await this.connect();
+    return await work();
+  }
+
+  async getStatuses(force = false): Promise<{ devId: string; dps: { [key: string]: string | boolean | number } }> {
+    if (force || !this.lastStatusUpdate || (new Date()).getTime() - this.lastStatusUpdate > this.maxStatusUpdateAge) {
+      return await this.doWork(async () => {
+        this.statuses = await this.api.get({ schema: true });
+        this.lastStatusUpdate = (new Date()).getTime();
+        return this.statuses as { devId: string; dps: { [key: string]: string | boolean | number } };
+      });
+    } else {
+      return this.statuses as { devId: string; dps: { [key: string]: string | boolean | number } };
+    }
+  }
+
+  async getCleanSpeed(force = false): Promise<CleanSpeed> {
+    const statuses = await this.getStatuses(force);
+    return <CleanSpeed>statuses.dps[this.CLEAN_SPEED];
+  }
+
+  async setCleanSpeed(cleanSpeed: CleanSpeed) {
+    await this.doWork(async () => {
+      await this.set({
+        [this.CLEAN_SPEED]: cleanSpeed,
+      });
+    });
+  }
+
+  async getPlayPause(force = false): Promise<boolean> {
+    const statuses = await this.getStatuses(force);
+    return <boolean>statuses.dps[this.PLAY_PAUSE];
+  }
+
+  async setPlayPause(state: boolean) {
+    await this.doWork(async () => {
+      await this.set({
+        [this.PLAY_PAUSE]: state,
+      });
+    });
+  }
+
+  async play() {
+    await this.setPlayPause(true);
+  }
+
+  async pause() {
+    await this.setPlayPause(true);
+  }
+
+  async getWorkMode(force = false): Promise<WorkMode> {
+    const statuses = await this.getStatuses(force);
+    return <WorkMode>statuses.dps[this.WORK_MODE];
+  }
+
+  async setWorkMode(workMode: WorkMode) {
+    await this.doWork(async () => {
+      if (this.debugLog) {
+        console.log(`Setting WorkMode to ${workMode}`);
+      }
+      await this.set({
+        [this.WORK_MODE]: workMode,
+      });
+    });
+  }
+
+  async startCleaning(force = false) {
+    if (this.debugLog) {
+      console.log('Starting Cleaning', JSON.stringify(await this.getStatuses(force), null, 4));
+    }
+    await this.setWorkMode(WorkMode.AUTO);
+
+    if (this.debugLog) {
+      console.log('Cleaning Started!');
+    }
+  }
+
+  async getWorkStatus(force = false): Promise<WorkStatus> {
+    const statuses = await this.getStatuses(force);
+    return <WorkStatus>statuses.dps[this.WORK_STATUS];
+  }
+
+  async setWorkStatus(workStatus: WorkStatus) {
+    await this.doWork(async () => {
+      await this.set({
+        [this.WORK_STATUS]: workStatus,
+      });
+    });
+  }
+
+  async goHome() {
+    await this.doWork(async () => {
+      await this.set({
+        [this.GO_HOME]: true,
+      });
+    });
+  }
+
+  async setDirection(direction: Direction) {
+    await this.doWork(async () => {
+      await this.set({
+        [this.DIRECTION]: direction,
+      });
+    });
+  }
+
+  async setFindRobot(state: boolean) {
+    return await this.doWork(async () => {
+      await this.set({
+        [this.FIND_ROBOT]: state,
+      });
+    });
+  }
+
+  async getFindRobot(force = false) {
+    const statuses = await this.getStatuses(force);
+    return <boolean>statuses.dps[this.FIND_ROBOT];
+  }
+
+  async getBatteryLevel(force = false) {
+    const statuses = await this.getStatuses(force);
+    return <number>statuses.dps[this.BATTERY_LEVEL];
+  }
+
+  async getErrorCode(force = false): Promise<ErrorCode> {
+    const statuses = await this.getStatuses(force);
+    return <ErrorCode>statuses.dps[this.ERROR_CODE];
+  }
+
+  async set(data: { [key: string]: string | number | boolean }) {
+    if (this.debugLog) {
+      console.log(`Setting: ${JSON.stringify(data, null, 4)}`);
+    }
+    return await this.api.set({
+      multiple: true,
+      data: data,
+    });
+  }
+
+  formatStatus() {
+    if (this.statuses && this.statuses.dps) {
+      console.log(`
+      -- Status Start --
+       - Play/Pause: ${(this.statuses.dps as any)[this.PLAY_PAUSE]}
+       - Direction: ${(this.statuses.dps as any)[this.DIRECTION]}
+       - Work Mode: ${(this.statuses.dps as any)[this.WORK_MODE]}
+       - Go Home: ${(this.statuses.dps as any)[this.GO_HOME]}
+       - Clean Speed: ${(this.statuses.dps as any)[this.CLEAN_SPEED]}
+       - Find Robot: ${(this.statuses.dps as any)[this.FIND_ROBOT]}
+       - Battery Level: ${(this.statuses.dps as any)[this.BATTERY_LEVEL]}
+       - Error Code: ${(this.statuses.dps as any)[this.ERROR_CODE]}
+      -- Status End --
+      `);
+    } else {
+      console.log('Statuses or dps is null');
+    }
+  }
+
+}
+
+export interface StatusResponse {
+    devId: string;
+    dps: {
+        '1': boolean;
+        '2': boolean;
+        '3': string;
+        '5': string;
+        '15': string;
+        '101': boolean;
+        '102': string;
+        '103': boolean;
+        '104': number;
+        '106': string;
+    };
+}
+
+export interface RoboVacConfig {
+    deviceId: string;
+    localKey: string;
+    ip?: string;
+    port?: number;
+}
+
+export enum CleanSpeed {
+    STANDARD = 'Standard',
+    BOOST_IQ = 'Boost_IQ',
+    MAX = 'Max',
+    NO_SUCTION = 'No_suction'
+}
+
+export enum WorkMode {
+    AUTO = 'auto',
+    SMALL_ROOM = 'SmallRoom',
+    SPOT = 'Spot',
+    EDGE = 'Edge',
+    NO_SWEEP = 'Nosweep'
+}
+
+export enum WorkStatus {
+    RUNNING = 'Running', // Cleaning
+    STAND_BY = 'standby', // Not in the dock, paused
+    SLEEPING = 'Sleeping', // Not in the dock - goes into this state after being paused for a while
+    CHARGING = 'Charging', // In the dock, charging
+    COMPLETED = 'completed', // In the dock, full charged
+    RECHARGE_NEEDED = 'Recharge' // Going home because battery is depleted or home was pressed
+}
+
+export enum Direction {
+    FORWARD = 'forward',
+    BACKWARD = 'back',
+    LEFT = 'left',
+    RIGHT = 'right'
+}
+
+export enum ErrorCode {
+    NO_ERROR = 'no_error',
+    STUCK_5_MIN = 'Stuck_5_min',
+    CRASH_BAR_STUCK = 'Crash_bar_stuck',
+    SENSOR_DIRTY = 'sensor_dirty',
+    NOT_ENOUGH_POWER = 'N_enough_pow',
+    WHEEL_STUCK = 'Wheel_stuck',
+    S_BRUSH_STUCK = 'S_brush_stuck',
+    FAN_STUCK = 'Fan_stuck',
+    R_BRUSH_STUCK = 'R_brush_stuck'
+}
